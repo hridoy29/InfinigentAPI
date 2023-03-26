ea317b9068bf4dfc2de5d0e4ffa263cc24dfc1c0
hridoy29 <jubairuva@gmail.com>
2023-03-26 12:21:48 +0600

    AssetDistributor API

---
+++ InfinigentBackend/SECURITY/SecurityEntity/AssetDistributor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfinigentBackend.SECURITY.SecurityEntity
+{
+     public class AssetDistributor
+    {
+        public string DistributorId { get; set; }
+        public string DistributorName { get; set; }
+
+
+    }
+}
